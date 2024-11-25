6ac5504901a5edd37a7304948ec237d0a5e3212e
Benjamin Skinner <bskinner@neh.gov>
2024-11-25 12:17:35 -0500

    updated batch to use command line arguments

--- batch.sh
+++ batch.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env sh
 
-for i in {2014..2023};
+# to run: call from command line using two arguments:
+#
+# $> ./batch < start YYYY > < end YYYY >
+#
+# e.g.,
+#
+# $> ./batch 2014 2023
+#
+# NB: using YYYY format for year:
+#
+# yes: 2023
+#  no: 23
+
+# first year is start; second year is end
+start=$1
+end=$2
+
+# loop through each year, calling ./build for that year
+for ((i=$start; i<=$end; i++));
 do
     echo "Building year: ${i}"
     ./build.sh ${i}
